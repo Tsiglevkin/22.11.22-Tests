526252d8dca12bf8d27e9498b40b6de4787ba8be
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2022-11-23 00:44:15 +0500

    initial commit

---
+++ Tests/test_unittest.py
@@ -0,0 +1,37 @@
+import unittest
+from function import find_country, kill_doubles
+# from main import geo_logs  # почему при импорте из любого другого файла импортируется пустой список?
+from main import ids
+
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+
+
+class TestFunc(unittest.TestCase):
+    def test_find_country(self):
+        result = find_country(some_list=geo_logs, country='Россия')
+        standart = [
+            {'visit1': ['Москва', 'Россия']},
+            {'visit3': ['Владимир', 'Россия']},
+            {'visit7': ['Тула', 'Россия']},
+            {'visit8': ['Тула', 'Россия']},
+            {'visit9': ['Курск', 'Россия']},
+            {'visit10': ['Архангельск', 'Россия']}
+        ]
+        self.assertEqual(result, standart)
+
+    def test_kill_doubles(self):
+        result = kill_doubles(some_dict=ids)
+        standart = [15, 35, 54, 98, 119, 213]
+        self.assertEqual(result, standart)
+
---
+++ function.py
@@ -0,0 +1,34 @@
+
+def find_country(some_list: list, country: str) -> list:
+    res_list = []
+    for visit in some_list:
+        for city, country_2 in visit.values():
+            if country_2 is country:
+                res_list.append(visit)
+    return res_list
+
+
+def kill_doubles(some_dict: dict) -> list:
+    temp = set()
+    for value in some_dict.values():
+        for num in value:
+            temp.add(num)
+    res = list(temp)
+    res.sort()
+    return res
+
+
+def make_range(some_list: list) -> dict:
+    res_dict = {}
+    for row in some_list:
+        temp = row.split()
+        key = f'длина в {len(temp)} слова'
+        res_dict.setdefault(key, 0)
+        res_dict[key] += 1
+    for key, value in res_dict.items():
+        temp = (value / len(some_list)) * 100
+        percent = f'{round(temp)} %'
+        res_dict[key] = percent
+    return res_dict
+
+
